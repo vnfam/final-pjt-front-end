bdc13c8a3e5fbd77a04ebf9329ead9e5d2e685d3
hyeongil <coguddlf1000@naver.com>
2024-09-05 23:44:19 +0900

    feat: add payment

--- src/components/payment/PaymentCompo.vue
+++ src/components/payment/PaymentCompo.vue
@@ -1,31 +1,50 @@
-<!-- <template>
+<template>
   <h1>결제 !</h1>
-  <button @click="paymentRequest()">결제</button>
 
+  <div>
+    <div v-for="membership in membershipList" :key="membership.memberShipId">
+      <p>Type: {{ membership.type }}</p>
+      <p>Price: {{ membership.price }}</p>
+      <button @click="paymentRequest(membership)">결제</button>
+    </div>
+  </div>
 </template>
 
 <script>
+
+import axios from 'axios';
 const SERVER_BASE_URL = 'http://localhost:8080';
 
-
 export default {
-  props: {
-    membershipList
+  
+  data() {
+    return {
+      membershipList: [],
+    }
   },
 
   mounted() {
-    SERVER_BASE_URL/memberships
+    // memberships를 호출해서 membershipList 조회
+    axios.get(`${SERVER_BASE_URL}/api/memberships`)
+      .then(response => {
+        console.log(response.data);
+        this.membershipList = response.data.data;
+        console.log(this.membershipList);
+      })
   },
 
   methods: {
-    paymentRequest(membershipId) {
+    paymentRequest(memberShip) {
+      // 1. 결제 정보를 생성한다.
+
+      // 2. 이후 PG사를 통해 실제 결제를 요청한다.
       window.IMP.request_pay(
         {
           pg: 'html5_inicis',
           pay_method: 'card',
-          merchant_uid: `payment-${crypto.randomUUID()}`, // 주문 고유 번호
-          name: `${membership_name}`,
-          amount: `${membership_price}`,
+          merchant_uid: `payment-${crypto.randomUUID()}`, // 서버 내부에서 생성한 주문 고유 번호
+          name: `${memberShip.type}`,
+          amount: `${memberShip.price}`,
           buyer_email: 'gildong@gmail.com',
           buyer_name: '홍길동',
           buyer_tel: '010-4242-4242',
@@ -38,18 +57,18 @@ export default {
           }
 
 
-          // 고객사 서버에서 /payment/complete 엔드포인트를 구현해야 합니다.
-          // (다음 목차에서 설명합니다)
-          // const notified = await fetch(`${SERVER_BASE_URL}/payment/complete`, {
-          //   method: 'POST',
-          //   headers: { 'Content-Type': 'application/json' },
-          //   // imp_uid와 merchant_uid, 주문 정보를 서버에 전달합니다
-          //   body: JSON.stringify({
-          //     imp_uid: response.imp_uid,
-          //     merchant_uid: response.merchant_uid,
-          //     // 주문 정보...
-          //   }),
-          // });
+//          고객사 서버에서 /payment/complete 엔드포인트를 구현해야 합니다.
+//          (다음 목차에서 설명합니다)
+          const notified = await fetch(`${SERVER_BASE_URL}/payment/complete`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            // imp_uid와 merchant_uid, 주문 정보를 서버에 전달합니다
+            body: JSON.stringify({
+              imp_uid: response.imp_uid,
+              merchant_uid: response.merchant_uid,
+              // 주문 정보...
+            }),
+          });
         }
       );
     },
@@ -57,4 +76,4 @@ export default {
 };
 </script>
 
-<style></style> -->
+<style></style>
