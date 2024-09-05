5de0a5264cee299c41c46e0f432a8adf1afa6319
이연수 <dldustn2040@gmail.com>
2024-09-05 10:53:57 +0900

    feat: Design Home Page

--- src/components/HomeCompo.vue
+++ src/components/HomeCompo.vue
@@ -1,9 +1,37 @@
 <template>
-  <div>Home</div>
+  <div>
+    <category-compo></category-compo>
+    <banner-compo></banner-compo>
+    <div class="pt-8 pb-4">
+      <p class="text-[22px] font-bold text-[#1d1d1d] leading-[22px] tracking-[-0.3px] mb-2 flex items-center">
+        시공전문가
+      </p>
+      <div class="flex items-center">
+        <font-awesome-icon class="w-4 h-4 mt-[2px] mr-[4px] text-[#555]" :icon="['fas', 'circle-info']" />
+        <span class="text-[14px] font-normal text-[#555] leading-[20px] tracking-[-0.3px]"
+          >선택한 지역의 시공전문가를 보여드립니다.</span
+        >
+      </div>
+    </div>
+    <div class="w-full flex items-center flex-wrap gap-[20px] pb-[40px]">
+      <company-card></company-card>
+      <company-card></company-card>
+      <company-card></company-card>
+      <company-card></company-card>
+      <company-card></company-card>
+    </div>
+    <to-top-compo></to-top-compo>
+  </div>
 </template>
 
 <script>
-export default {};
+import CategoryCompo from './common/CategoryCompo.vue';
+import ToTopCompo from './common/ToTopCompo.vue';
+import BannerCompo from './company/BannerCompo.vue';
+import CompanyCard from './company/CompanyCard.vue';
+export default {
+  components: { CategoryCompo, BannerCompo, CompanyCard, ToTopCompo },
+};
 </script>
 
 <style></style>
---
+++ src/components/common/ToTopCompo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="w-[64px] h-[64px] fixed right-4 bottom-[80px] transition-all duration-300 cursor-pointer bg-no-repeat bg-cover"
+    style="background-image: url('@/assets/ic_arrow_top.svg'); background-position: 100% 100%"
+    data-nav="true"
+  >
+    <img src="@/assets/ic_arrow_top.svg" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
