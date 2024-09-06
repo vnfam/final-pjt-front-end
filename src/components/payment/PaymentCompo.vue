3f84f11255c9846d2fe5066ef42364a0668941b8
Chae-1 <coguddlf1000@naver.com>
2024-09-06 17:57:26 +0900

    feat: add payment for membership

--- src/components/payment/PaymentCompo.vue
+++ src/components/payment/PaymentCompo.vue
@@ -1,31 +1,49 @@
-<!-- <template>
-  <h1>결제 !</h1>
-  <button @click="paymentRequest()">결제</button>
-
+<template>
+  <div>
+    <div v-for="membership in membershipList" :key="membership.id">
+      <div>{{ membership.type }}</div>
+      <div>{{ membership.price }}</div>
+      <button @onclick="paymentRequest(membership.id)">결제하기</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 const SERVER_BASE_URL = 'http://localhost:8080';
 
-
 export default {
-  props: {
-    membershipList
+  data() {
+    return {
+      membershipList: [],
+    };
   },
 
   mounted() {
-    SERVER_BASE_URL/memberships
+    axios.get(`${SERVER_BASE_URL}/api/memberships`)
+      .then(response => {
+        this.membershipList = response.data.data;
+        this.membershipList.forEach(item => console.log(item));
+      });
   },
 
   methods: {
-    paymentRequest(membershipId) {
+    async paymentRequest(membershipId) {
+     // 향후 Authorization Header 전송을 수행해야한다.
+      const response = await axios.post(`${SERVER_BASE_URL}/api/payment/prepare`, {
+        membershipId: membershipId,
+      });
+      console.log(response);
+
       window.IMP.request_pay(
         {
           pg: 'html5_inicis',
           pay_method: 'card',
-          merchant_uid: `payment-${crypto.randomUUID()}`, // 주문 고유 번호
-          name: `${membership_name}`,
-          amount: `${membership_price}`,
+          merchant_uid: '', // 주문 고유 번호
+          name: `${response.data.type}`,
+          amount: `${response.data}`,
+
           buyer_email: 'gildong@gmail.com',
           buyer_name: '홍길동',
           buyer_tel: '010-4242-4242',
@@ -33,11 +51,17 @@ export default {
           buyer_postcode: '01181',
         },
         async (response) => {
+          console.log(response);
           if (response.error_code != null) {
             return alert(`결제에 실패하였습니다. 에러 내용: ${response.error_msg}`);
           }
 
+          const complete = await axios.post(`${SERVER_BASE_URL}/api/payment/complete`, {
+            impUid: response.imp_uid,
+            merchantUid: response.merchant_uid,
+          });
 
+          console.log(complete);
           // 고객사 서버에서 /payment/complete 엔드포인트를 구현해야 합니다.
           // (다음 목차에서 설명합니다)
           // const notified = await fetch(`${SERVER_BASE_URL}/payment/complete`, {
@@ -50,11 +74,13 @@ export default {
           //     // 주문 정보...
           //   }),
           // });
-        }
+
+          throw new Error(response);
+        },
       );
     },
   },
 };
 </script>
 
-<style></style> -->
+<style></style>
