ca639dd748aa75808ce6fff96bcafd8a12388004
Chae-1 <coguddlf1000@naver.com>
2024-09-13 08:23:02 +0900

    feat: refresh accesstoken with refreshtoken

--- src/components/payment/PaymentCompo.vue
+++ src/components/payment/PaymentCompo.vue
@@ -56,52 +56,42 @@ export default {
 
       // 향후 Authorization Header 전송을 수행해야한다.
 
-      try {
-        const response = await authInstance.post(`/api/payment/prepare`, {
-          membershipId: membershipId,
-        });
+      const response = await authInstance.post(`/api/payment/prepare`, {
+        membershipId: membershipId,
+      });
 
-        const price = response.data.data.price;
-        const membershipType = response.data.data.type;
-        const merchantUid = response.data.data.merchantUid;
+      const price = response.data.data.price;
+      const membershipType = response.data.data.type;
+      const merchantUid = response.data.data.merchantUid;
 
-        console.log(price);
-        console.log(response);
-        console.log('토큰 정보 : ' + this.token);
+      console.log(price);
+      console.log(response);
+      console.log('토큰 정보 : ' + this.token);
 
-        window.IMP.request_pay(
-          {
-            pg: 'html5_inicis',
-            pay_method: 'card',
-            merchant_uid: `${merchantUid}`, // 주문 고유 번호
-            name: `${membershipType}`,
-            amount: `${price}`,
+      window.IMP.request_pay(
+        {
+          pg: 'html5_inicis',
+          pay_method: 'card',
+          merchant_uid: `${merchantUid}`, // 주문 고유 번호
+          name: `${membershipType}`,
+          amount: `${price}`,
 
-            buyer_email: 'gildong@gmail.com',
-            buyer_name: '홍길동',
-            buyer_tel: '010-4242-4242',
-            buyer_addr: '서울특별시 강남구 신사동',
-            buyer_postcode: '01181',
-          },
+          buyer_email: 'gildong@gmail.com',
+          buyer_name: '홍길동',
+          buyer_tel: '010-4242-4242',
+          buyer_addr: '서울특별시 강남구 신사동',
+          buyer_postcode: '01181',
+        },
 
-          async (response) => {
+        async (response) => {
+          try {
             console.log(response);
-
-            // 성공시 호출
-            const complete = await authInstance.post(
-              `${SERVER_BASE_URL}/api/memberships`,
-              {
-                impUid: response.imp_uid,
-                merchantUid: response.merchant_uid,
-                success: response.success,
-                paidAmount: response.paid_amount,
-              },
-              {
-                headers: {
-                  Authorization: this.token,
-                },
-              }
-            );
+            const complete = await authInstance.post(`${SERVER_BASE_URL}/api/memberships`, {
+              impUid: response.imp_uid,
+              merchantUid: response.merchant_uid,
+              success: response.success,
+              paidAmount: response.paid_amount,
+            });
 
             const paymentResult = complete.data.data;
 
@@ -110,24 +100,24 @@ export default {
             if (paymentResult) {
               // 어떤걸 할지 결정해야함.
             }
-
-            // 고객사 서버에서 /payment/complete 엔드포인트를 구현해야 합니다.
-            // (다음 목차에서 설명합니다)
-            // const notified = await fetch(`${SERVER_BASE_URL}/payment/complete`, {
-            //   method: 'POST',
-            //   headers: { 'Content-Type': 'application/json' },
-            //   // imp_uid와 merchant_uid, 주문 정보를 서버에 전달합니다
-            //   body: JSON.stringify({
-            //     imp_uid: response.imp_uid,
-            //     merchant_uid: response.merchant_uid,
-            //     // 주문 정보...
-            //   }),
-            // });
+          } catch (error) {
+            console.log(error);
           }
-        );
-      } catch (error) {
-        console.log(error);
-      }
+
+          // 고객사 서버에서 /payment/complete 엔드포인트를 구현해야 합니다.
+          // (다음 목차에서 설명합니다)
+          // const notified = await fetch(`${SERVER_BASE_URL}/payment/complete`, {
+          //   method: 'POST',
+          //   headers: { 'Content-Type': 'application/json' },
+          //   // imp_uid와 merchant_uid, 주문 정보를 서버에 전달합니다
+          //   body: JSON.stringify({
+          //     imp_uid: response.imp_uid,
+          //     merchant_uid: response.merchant_uid,
+          //     // 주문 정보...
+          //   }),
+          // });
+        }
+      );
     },
     async refundRequest() {
       const result = await authInstance.post(`${SERVER_BASE_URL}/api/membership`, {
---
+++ src/components/payment/PaymentSuccess.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>결제 성공</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
