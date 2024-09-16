a769d0d9111f512c5771d08c01fafa43af8c5673
이연수 <dldustn2040@gmail.com>
2024-09-16 23:05:39 +0900

    fix: Remove content to portfolio list

--- src/components/portfolio/PortfolioCard.vue
+++ src/components/portfolio/PortfolioCard.vue
@@ -4,7 +4,6 @@
     class="w-[353px] h-[354px] border-solid border-[1px] border-[#eee] rounded-[8px] cursor-pointer border-box hover:shadow-md"
   >
     <p>{{ portfolio.title }}</p>
-    <p v-html="sanitizedContent"></p>
     <p>{{ portfolio.projectArea }}</p>
     <p>{{ portfolio.projectLocation }}</p>
     <p>{{ portfolio.buildingType }}</p>
@@ -12,20 +11,12 @@
 </template>
 
 <script>
-import DOMPurify from 'dompurify';
-
 export default {
   props: {
     portfolio: {
       type: Object,
       required: true,
       default: () => ({}),
-    },
-  },
-  computed: {
-    sanitizedContent() {
-      // DOMPurify로 content를 필터링
-      return DOMPurify.sanitize(this.portfolio.content);
     },
   },
   methods: {
