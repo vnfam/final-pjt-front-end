be7b60b2fd3e5207328797f14e42bb49a22bc492
woodongle <315ehdgur@gmail.com>
2024-09-17 14:15:22 +0900

    feat: estimateList, companyList

--- src/router/route.js
+++ src/router/route.js
@@ -19,6 +19,7 @@ import UserBookMark from '@/components/mypage/user/UserBookMark.vue';
 import UserReviewList from '@/components/mypage/user/review/UserReviewList.vue';
 import EstimateList from '@/components/estimate/EstimateList.vue';
 import VerifyCompo from '@/components/login/VerifyCompo.vue';
+import StepSix from '@/components/estimate/StepSix.vue';
 
 export default [
   { path: '/', component: HomeCompo },
@@ -36,6 +37,7 @@ export default [
       { path: 'stepThree', component: StepThree },
       { path: 'stepFour', component: StepFour },
       { path: 'stepFive', component: StepFive },
+      { path: 'stepSix', component: StepSix },
     ],
   },
   { path: '/payment', component: PaymentCompo },
