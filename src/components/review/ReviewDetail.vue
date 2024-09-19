a4147338fa6a981e4b97ff4de35e7a12b80b91fc
vnfma <vnfam0626@gmail.com>
2024-09-19 16:34:14 +0900

    fix: reviewList, Card, Detail page connect serverData

--- src/components/review/ReviewDetail.vue
+++ src/components/review/ReviewDetail.vue
@@ -2,34 +2,29 @@
   <div class="flex flex-col px-20 py-10">
     <div class="my-carousel">
       <swiper
+        v-if="reviewDetail.reviewImageResponses.length > 1"
         :modules="[Navigation, Pagination]"
         :navigation="true"
         :pagination="{ clickable: true }"
         :loop="true"
         class="h-[400px] w-[1100px] rounded-[8px]"
       >
-        <swiper-slide>
+        <swiper-slide v-for="(image, index) in reviewDetail.reviewImageResponses" :key="index">
           <img
             class="w-full h-full object-fill"
-            src="https://cf.zipdoc.co.kr/zws/res/assets/images/zipdoc_v35/common/banner_lx_long2.png"
-            alt="banner1"
-          />
-        </swiper-slide>
-        <swiper-slide>
-          <img
-            class="w-full h-full object-fill"
-            src="https://cf.zipdoc.co.kr/zws/res/assets/images/zipdoc_v35/common/home-banner-pc-01.jpg"
-            alt="banner2"
-          />
-        </swiper-slide>
-        <swiper-slide>
-          <img
-            class="w-full h-full object-fill"
-            src="https://cf.zipdoc.co.kr/zws/res/assets/images/zipdoc_v35/common/home-banner-pc-04.jpg"
-            alt="banner3"
+            :src="image.imageUrl || require('@/assets/replaceHouse.png')"
+            alt="Review Image"
           />
         </swiper-slide>
       </swiper>
+
+      <!-- When there is only 1 image, display it without the Swiper -->
+      <img
+        v-else
+        class="w-full h-[400px] rounded-[8px] object-fill"
+        :src="reviewDetail.reviewImageResponses[0]?.imageUrl || require('@/assets/replaceHouse.png')"
+        alt="Review Image"
+      />
     </div>
 
     <div class="flex flex-col mt-10 rounded border-4 rounded-lg">
@@ -37,17 +32,25 @@
       <ul class="flex flex-row justify-between my-5 text-lg">
         <ul class="pl-40">
           업체정보
-          <li class="mt-3">{{ reviewDetail.rating }}점</li>
+          <li class="mt-3">
+            <font-awesome-icon class="text-midGreen" :icon="['fas', 'star']" />&nbsp; {{ reviewDetail.rating }}점
+          </li>
           <li>{{ reviewDetail.companyName }}</li>
-          <li>도배 / 샷시</li>
-          <li>2주</li>
+          <li>
+            <!-- constuctionType이 배열 그자체로 나오지 않게 하려고  -->
+            <span v-for="(constructionType, index) in reviewDetail.reviewConstructionTypeResponses" :key="index">
+              {{ constructionType.constructionType }}
+              <span v-if="index < reviewDetail.reviewConstructionTypeResponses.length - 1">, </span>
+            </span>
+          </li>
+          <li>{{ schedule }}</li>
         </ul>
         <ul class="pr-40">
           고객정보
           <li class="mt-3">{{ reviewDetail.memberNickName }}</li>
-          <li>아파트 / {{ reviewDetail.floor }}</li>
+          <li>{{ reviewDetail.buildingTypeName }} / {{ reviewDetail.floor }}</li>
           <li>{{ reviewDetail.totalPrice }} 만원</li>
-          <li>2024.09.11</li>
+          <li>{{ formattedRegDate }}</li>
         </ul>
       </ul>
     </div>
@@ -79,11 +82,12 @@ export default {
         memberNickName: '',
         workStartDate: '',
         workEndDate: '',
-        constructionType: '',
+        buildingTypeName: '',
+        reviewConstructionTypeResponses: [],
         totalPrice: '',
         regDate: '',
         updatable: false,
-        images: [],
+        reviewImageResponses: [],
       },
     };
   },
@@ -91,6 +95,47 @@ export default {
   components: {
     Swiper,
     SwiperSlide,
+  },
+
+  computed: {
+    // 게시일에 불필요한 시간 자르기
+    formattedRegDate() {
+      if (!this.reviewDetail || !this.reviewDetail.regDate) return '';
+
+      const date = new Date(this.reviewDetail.regDate);
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const day = String(date.getDate()).padStart(2, '0');
+
+      // Return the formatted date as YYYY.MM.DD
+      return `${year}.${month}.${day}`;
+    },
+
+    // 시공 기간 계산하기
+    schedule() {
+      if (!this.reviewDetail.workStartDate || !this.reviewDetail.workEndDate) return '';
+
+      const startDate = new Date(this.reviewDetail.workStartDate);
+      const endDate = new Date(this.reviewDetail.workEndDate);
+
+      if (
+        startDate.getFullYear() === endDate.getFullYear() &&
+        startDate.getMonth() === endDate.getMonth() &&
+        startDate.getDate() === endDate.getDate()
+      ) {
+        return '1 일';
+      }
+
+      const timeDiff = Math.abs(endDate - startDate);
+      const dayDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
+      if (dayDiff >= 7) {
+        const weekDiff = Math.ceil(dayDiff / 7);
+        return `${weekDiff} 주`;
+      } else {
+        return `${dayDiff} 일`;
+      }
+    },
   },
 
   async mounted() {
@@ -113,10 +158,13 @@ export default {
     this.reviewDetail.floor = reviewDetail.floor;
     this.reviewDetail.workStartDate = reviewDetail.workStartDate;
     this.reviewDetail.workEndDate = reviewDetail.workEndDate;
+    this.reviewDetail.buildingTypeName = reviewDetail.buildingTypeName;
+    this.reviewDetail.reviewConstructionTypeResponses = reviewDetail.reviewConstructionTypeResponses;
     this.reviewDetail.regDate = reviewDetail.regDate;
     this.reviewDetail.totalPrice = reviewDetail.totalPrice;
     this.reviewDetail.memberNickName = reviewDetail.memberNickName;
     this.reviewDetail.updatable = reviewDetail.updatable;
+    this.reviewDetail.reviewImageResponses = reviewDetail.reviewImageResponses;
     console.log(reviewDetail);
 
     console.log(this.reviewDetail.updatable);
