d819e59886e3771558c898b8c09ead065e5aaa16
Kim Donghyeok <315ehdgur@gmail.com>
2024-09-19 14:56:20 +0900

    fix: EstimateList feat: mypage/company

---
+++ src/components/mypage/company/CompanyPageCompo.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="container mx-auto p-6">
+    <!-- Sidebar -->
+    <div class="flex max-w-7xl">
+      <div class="basis-1/4 bg-white shadow-md rounded-lg p-4">
+        <h2 class="text-lg font-semibold mb-4">마이페이지</h2>
+        <ul class="text-gray-800">
+          <!-- 기본정보 -->
+          <li class="mb-2">
+            <button
+              @click="activeTab = 'info'"
+              :class="{'bg-gray-200': activeTab === 'info'}"
+              class="block p-2 rounded hover:bg-gray-200 w-full text-left"
+            >
+              기본정보
+            </button>
+          </li>
+          <!-- 견적내역 -->
+          <li class="mb-2">
+            <button
+              @click="toggleRequestList"
+              class="block w-full text-left p-2 rounded hover:bg-gray-200"
+            >
+              견적내역
+            </button>
+            <!-- 보낸 요청, 받은 요청 토글 버튼 -->
+            <div v-if="showRequestOptions" class="ml-4 mt-2">
+              <button
+                @click="activeTab = 'received'"
+                :class="{'bg-gray-200': activeTab === 'received'}"
+                class="block w-full text-left p-2 rounded hover:bg-gray-100 mt-2"
+              >
+                받은 요청
+              </button>
+              <button
+                @click="activeTab = 'sent'"
+                :class="{'bg-gray-200': activeTab === 'sent'}"
+                class="block w-full text-left p-2 rounded hover:bg-gray-100"
+              >
+                보낸 요청
+              </button>
+            </div>
+          </li>
+          <!-- 시공사례내역 -->
+          <li class="mb-2">
+            <button
+              @click="activeTab = 'portfolio'"
+              :class="{'bg-gray-200': activeTab === 'portfolio'}"
+              class="block p-2 rounded hover:bg-gray-200 w-full text-left"
+            >
+              시공사례내역
+            </button>
+          </li>
+        </ul>
+      </div>
+
+      <!-- Main Content Area -->
+      <div class="basis-3/4 bg-white shadow-2xl rounded-lg p-6 ml-4">
+        <!-- 동적 컴포넌트 렌더링 -->
+        <component :is="activeComponent"></component>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CompanyBaseInfo from './CompanyBaseInfo.vue';
+import ReceivedRequests from './ReceivedRequests.vue';
+import CompanyPortfolioList from './portfolio/CompanyPortfolioList.vue';
+import SentRequests from './SentRequests.vue';
+
+export default {
+  data() {
+    return {
+      showRequestOptions: false, // 견적내역 클릭 시 보낸/받은 요청 버튼을 보여줄지 여부
+      activeTab: 'info', // 기본값은 info 컴포넌트
+    };
+  },
+  computed: {
+    // activeTab에 따라 보여줄 컴포넌트 선택
+    activeComponent() {
+      switch (this.activeTab) {
+        case 'info':
+          return CompanyBaseInfo;
+        case 'sent':
+          return SentRequests;
+        case 'received':
+          return ReceivedRequests;
+        case 'portfolio':
+          return CompanyPortfolioList;
+        default:
+          return CompanyBaseInfo;
+      }
+    },
+  },
+  methods: {
+    toggleRequestList() {
+      this.showRequestOptions = !this.showRequestOptions; // 견적내역 버튼 클릭 시 토글
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/mypage/company/ReceivedRequests.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+      <h3 class="font-medium text-[18px] mb-4">견적 요청 목록</h3>
+      <ul>
+        <li v-for="(estimate, index) in estimates" :key="index" class="bg-gray-100 mb-4 p-4 shadow rounded">
+          <p><strong>Requested by:</strong> {{ estimate.username }}</p>
+          <p><strong>Request Date:</strong> {{ estimate.requestDate }}</p>
+          <p><strong>Building Type:</strong> {{ estimate.buildingType }}</p>
+          <p><strong>Construction Types:</strong> {{ estimate.constructionTypes.join(', ') }}</p>
+          <p><strong>Budget:</strong> {{ estimate.budget }}</p>
+          <p><strong>Schedule:</strong> {{ estimate.schedule }}</p>
+          <p><strong>Address:</strong> {{ estimate.constructionAddress }}</p>
+          <p><strong>Floor:</strong> {{ estimate.floor }}</p>
+          <div class="text-right">
+            <button class="mr-4 bg-white rounded-xl py-2 px-4" @click="cancelEstimate(estimate.estimateRequestId)">
+              거절
+            </button>
+            <button
+              class="bg-midGreen text-white rounded-xl py-2 px-4"
+              @click="approvalEstimate(estimate.estimateRequestId)"
+            >
+              승인
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import authInstance from '@/utils/axiosUtils';
+  
+  export default {
+    data() {
+      return {
+        selectedRegion: '서울 강남구',
+        estimates: [],
+      };
+    },
+    created() {
+      this.fetchEstimates();
+    },
+    methods: {
+      async fetchEstimates() {
+        try {
+          const response = await authInstance.get('/api/estimates/list');
+          this.estimates = response.data.data; // API의 응답 데이터에 맞춰 수정
+        } catch (error) {
+          console.error('견적 리스트를 가져오는데 실패했습니다.', error);
+        }
+      },
+  
+      async cancelEstimate(estimateRequestId) {
+        const comfirmed = confirm('정말로 취소하시겠습니까?');
+        if (comfirmed) {
+          try {
+            await authInstance.post(`/api/estimates/cancel/${estimateRequestId}`);
+            // 취소 후 견적 리스트를 다시 가져와서 화면 갱신
+            this.fetchEstimates();
+          } catch (error) {
+            console.error('견적 취소에 실패했습니다.', error);
+          }
+        }
+      },
+  
+      async approvalEstimate(estimateRequestId) {
+        try {
+          alert('승인되었습니다.');
+          await authInstance.post(`/api/estimates/approval/${estimateRequestId}`);
+          // 취소 후 견적 리스트를 다시 가져와서 화면 갱신
+          this.fetchEstimates();
+        } catch (error) {
+          console.error('견적 승인에 실패했습니다.', error);
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* 스타일은 필요에 따라 추가 */
+  </style>
+  
