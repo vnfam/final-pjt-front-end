cb543a049a29e44d50effbcd15219a4f37ab22fd
이연수 <dldustn2040@gmail.com>
2024-09-23 12:32:46 +0900

    feat: design review

--- src/components/review/ReviewCard.vue
+++ src/components/review/ReviewCard.vue
@@ -6,30 +6,30 @@
       <h2 class="text-xl font-bold text-gray-900 mb-4">{{ review.title }}</h2>
 
       <!-- 내용 미리보기 -->
-      <div class="text-gray-700 mb-4">
+      <div class="text-gray-700">
         <p v-html="truncatedContent"></p>
       </div>
 
       <!-- 시공 정보 -->
-      <div class="mt-3">
+      <div class="mt-1">
         <ul class="flex flex-wrap text-sm text-gray-700">
-          <li class="px-3 py-1 bg-gray-200 rounded-full mr-2">{{ review.buildingType }}</li>
-          <li class="px-3 py-1 bg-gray-200 rounded-full mr-2">
+          <li class="px-3 py-1 text-[13px] bg-neutral rounded-full mr-2">{{ review.buildingType }}</li>
+          <li class="px-3 py-1 text-[13px] bg-neutral rounded-full mr-2">{{ review.floor }}평</li>
+          <li class="px-3 py-1 text-[13px] bg-neutral rounded-full mr-2">
             <span v-for="(constructionType, index) in review.constructionTypes" :key="index">
               {{ constructionType }}<span v-if="index !== review.constructionTypes.length - 1">, </span>
             </span>
           </li>
-          <li class="px-3 py-1 bg-gray-200 rounded-full mr-2">{{ review.floor }}평</li>
-          <li class="px-3 py-1 bg-gray-200 rounded-full">{{ schedule }}</li>
+          <li class="px-3 py-1 text-[13px] bg-neutral rounded-full">{{ schedule }}</li>
         </ul>
       </div>
 
       <!-- 별점 및 작성자 정보 -->
-      <div class="flex items-center mt-3 text-sm text-gray-600">
+      <div class="flex items-center mt-3 text-sm text-gray-600 font-medium">
         <font-awesome-icon class="text-midGreen mr-1" :icon="['fas', 'star']" />
         <span class="mr-4">{{ review.rating }}</span>
         <span class="mr-4">{{ review.memberNickName }} 고객님</span>
-        <span>{{ formattedRegDate }}</span>
+        <span class="font-normal text-gray-500">{{ formattedRegDate }}</span>
       </div>
     </div>
 
@@ -64,7 +64,7 @@ export default {
         return '';
       }
       const cleanedContent = this.review.content.replace(/<img[^>]*>/g, ''); // content에 img태그 지우기
-      return cleanedContent.length > 150 ? cleanedContent.substring(0, 150) + '...' : cleanedContent;
+      return cleanedContent.length > 100 ? cleanedContent.substring(0, 100) + '...' : cleanedContent;
     },
 
     // 게시일에 불필요한 시간 자르기
@@ -99,9 +99,9 @@ export default {
 
       if (dayDiff >= 7) {
         const weekDiff = Math.ceil(dayDiff / 7);
-        return `${weekDiff} 주`;
+        return `${weekDiff}주 소요`;
       } else {
-        return `${dayDiff} 일`;
+        return `${dayDiff}일 소요`;
       }
     },
   },
