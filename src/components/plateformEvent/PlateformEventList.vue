91f7ae4f1b9766d01dc58747028e480b06f82a48
vnfma <vnfam0626@gmail.com>
2024-09-24 22:29:22 +0900

    fix: Plateformevent List done, detail can not connect card. i will do it nextday

--- src/components/plateformEvent/PlateformEventList.vue
+++ src/components/plateformEvent/PlateformEventList.vue
@@ -1,22 +1,16 @@
 <template>
   <div class="py-5">
-    <p v-if="plateformEvent && plateformEvent.length" class="text-lg font-medium mb-6">
-      총
-      <strong class="text-xl">{{ plateformEvent.length }}개의 공지사항 수</strong>
-    </p>
+    <!-- <p v-if="plateformEvent && plateformEvent.length" class="text-lg font-medium mb-6"> -->
+    <strong class="text-xl">총 1개의 공지사항 수</strong>
+    <!-- </p> -->
 
-    <p v-else class="pt-20 text-center text-gray-600 text-lg font-medium">공지사항이 없습니다.</p>
+    <!-- <p v-else class="pt-20 text-center text-gray-600 text-lg font-medium">공지사항이 없습니다.</p> -->
   </div>
 
   <div class="flex flex-col space-y-6">
-    <plateformEventCard
-      v-for="plateformEvent in plateformEvent"
-      :key="plateformEvent.id"
-      :plateformEvent="plateformEvent"
-      class="p-6 bg-white rounded-lg"
-    ></plateformEventCard>
+    <plateformEventCard class="p-6 bg-white rounded-lg"></plateformEventCard>
 
-    <div v-if="plateformEvent.length > 0" class="flex justify-center">
+    <div class="flex justify-center">
       <button
         class="py-4 px-20 my-10 font-medium text-midGreen border-[1px] border-midGreen rounded hover:bg-midGreen hover:text-white transition duration-300"
       >
@@ -33,10 +27,12 @@ import PlateformEventCard from './PlateformEventCard.vue';
 export default {
   data() {
     return {
+      id: 1,
       plateformEvent: [],
     };
   },
 
+  // 서버 연결 준비
   // async mounted() {
   //   const plateformEventList = await axios.get('http://localhost:8080/api/plateformEvent/list');
   //   this.plateformEvent = plateformEventList.data;
