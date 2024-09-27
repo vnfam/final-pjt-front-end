b75f23b1c3d571ee7bc22697d7bfd0495ef6330b
Kim Donghyeok <315ehdgur@gmail.com>
2024-09-27 18:09:13 +0900

    feat: company membership

---
+++ src/components/mypage/company/CompanyMembership.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="p-4 max-w-screen-lg mx-auto">
+    <div class="mb-6">
+      <h3 class="font-bold text-2xl text-gray-800">멤버십 내역</h3>
+    </div>
+    <button @click="this.$router.push('/payment')" class="bg-midGreen text-white rounded-lg py-2 px-4 font-medium">
+      멤버십가입
+    </button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
