0bd2a7e71097e68e36dc7ac9b1999c16914733d1
vnfma <vnfam0626@gmail.com>
2024-10-01 15:28:53 +0900

    fix: AdminCompanyPortfolioList add moving data?..and pagenation(it will fix)

--- src/components/admin/company/AdminCompanyPortfolioList.vue
+++ src/components/admin/company/AdminCompanyPortfolioList.vue
@@ -5,12 +5,12 @@
       <h2 class="font-semibold text-xl mb-4">시공사례 관리</h2>
       <ul class="flex gap-4">
         <li>
-          <label for="" class="font-medium">현재 총 시공 사례수</label>
-          <p class="text-red">4명</p>
+          <label for="" class="font-medium">총 시공 사례수</label>
+          <p class="text-red">{{ portfolioCompany.length }}명</p>
         </li>
         <li>
           <label for="" class="font-medium">현재 등록된 시공 사례수</label>
-          <p class="text-red">4개</p>
+          <p class="text-red">{{ portfolioCompany.length }}개</p>
         </li>
       </ul>
     </div>
@@ -22,58 +22,26 @@
             <th class="bg-gray-200 text-center p-2 whitespace-nowrap">번호</th>
             <th class="bg-gray-200 text-center p-2 whitespace-nowrap">제목</th>
             <th class="bg-gray-200 text-center p-2 whitespace-nowrap">업체명</th>
-            <th class="bg-gray-200 text-center p-2 whitespace-nowrap">고객명</th>
             <th class="bg-gray-200 text-center p-2 whitespace-nowrap">게시일</th>
             <th class="bg-gray-200 text-center p-2 whitespace-nowrap">상세보기</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">1</td>
+          <tr v-for="(portfolio, index) in portfolioCompany" :key="index">
+            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">{{ index + 1 }}</td>
             <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">
-              편안하고 심플한 화이트&블랙 스타일
+              {{ portfolio.title }}
             </td>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">부자업체</td>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">김선우</td>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">24.09.29</td>
+            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">
+              {{ portfolio.companyName }}
+            </td>
+            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">
+              {{ portfolio.createdAt }}
+            </td>
             <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">
               <button
                 class="px-2 rounded-lg whitespace-nowrap bg-gray-200 cursor-pointer hover:bg-gray-300"
-                @click="$router.push('adminCompanyPortfolioDetail')"
-              >
-                상세보기
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">2</td>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">
-              따뜻하고 편안한 분위기 24평
-            </td>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">프리미엄업체</td>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">몰랑이</td>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">24.09.20</td>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">
-              <button
-                class="px-2 rounded-lg whitespace-nowrap bg-gray-200 cursor-pointer hover:bg-gray-300"
-                @click="$router.push('adminCompanyPortfolioDetail')"
-              >
-                상세보기
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">3</td>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">
-              설레이는 신혼부부의 첫시작
-            </td>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">뚝딱뚝딱업체</td>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">토심이와토뭉이</td>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">24.9.17</td>
-            <td class="text-center p-2 border-t border-gray-300 bg-white whitespace-nowrap">
-              <button
-                class="px-2 rounded-lg whitespace-nowrap bg-gray-200 cursor-pointer hover:bg-gray-300"
-                @click="$router.push('adminCompanyPortfolioDetail')"
+                @click="$router.push({ name: 'adminCompanyPortfolioDetail', params: { id: portfolio.id } })"
               >
                 상세보기
               </button>
@@ -83,13 +51,13 @@
       </table>
     </div>
     <!-- footer -->
-    <div class="mt-5">
+    <div class="mt-5" v-if="totalPage > 1">
       <vue-paginate
-        :model-value="page"
-        :page-count="20"
+        :model-value="pageNumber"
+        :page-count="totalPage"
         :page-range="3"
         :margin-pages="2"
-        :click-handler="clickCallback"
+        :click-handler="fetchPortfolioList"
         prev-text="<"
         next-text=">"
         :container-class="'flex justify-center font-sans cursor-pointer'"
@@ -97,7 +65,7 @@
         :prev-link-class="'m-3'"
         :next-link-class="'m-3'"
         active-class="bg-accent rounded-md"
-        @update:model-value="page = $event"
+        @update:model-value="pageNumber = $event"
       />
     </div>
   </div>
@@ -106,21 +74,43 @@
 <script>
 import { defineComponent, ref } from 'vue';
 import { VuePaginate } from '@svifty7/vue-paginate';
+import authInstance from '@/utils/axiosUtils';
 
 export default defineComponent({
   components: {
     VuePaginate,
   },
-  setup() {
-    const pageSize = ref(10);
 
-    const clickCallback = (pageNum) => {
-      console.log(pageNum);
+  setup() {
+    const pageNumber = ref(1);
+    const pageSize = ref(5);
+    const portfolioCompany = ref([]);
+    const totalPage = ref(1);
+
+    const fetchPortfolioList = async (pageNum = pageNumber.value) => {
+      try {
+        const response = await authInstance.get(`/api/admin/portfolios`, {
+          params: {
+            pageNumber: pageNum,
+            size: pageSize.value,
+          },
+        });
+        portfolioCompany.value = response.data.slice;
+        totalPage.value = response.data.totalPages;
+        pageNumber.value = response.data.number;
+      } catch (error) {
+        console.error('Failed to fetch portfolio data', error);
+      }
     };
 
+    fetchPortfolioList();
+
     return {
+      portfolioCompany,
+      pageNumber,
       pageSize,
-      clickCallback,
+      totalPage,
+      fetchPortfolioList,
     };
   },
 });
